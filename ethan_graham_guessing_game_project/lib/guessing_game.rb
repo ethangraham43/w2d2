52d897923e0227b5293d0ae3224f0b17feaeb541
Ethan Graham <ethangraham43@gmail.com>
2022-09-22 11:02:22 -0400

    Adding all files from w2d2

--- ethan_graham_guessing_game_project/lib/guessing_game.rb
+++ ethan_graham_guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,33 @@
 class GuessingGame
+    def initialize(min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
 
+    def num_attempts
+        @num_attempts
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(n)
+        @num_attempts += 1
+        if @secret_num == n
+            p "you win"
+            @game_over = true
+        elsif n > @secret_num
+            p "too big"
+        elsif n < @secret_num
+            p "too small"
+        end
+    end
+
+    def ask_user
+        p "enter a number"
+        user_n = gets.chomp.to_i
+        check_num(user_n)
+    end
 end
